74b0d75788ec322d0afb5bbb334c88c3caec02ba
PUBPOL 2130 Admin <pubpol.21.30@gmail.com>
2025-01-29 00:03:35 -0500

    Add support library

---
+++ lib/pubpol2130/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pubpol2130",
+    version="0.0.1",
+    url="https://github.com/PUBPOL-2130/notebooks",
+    install_requires=["census >= 0.8.19", "us >= 2.0.2", "pandas >= 2.2.3"],
+)
