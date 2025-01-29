59463cc754c8eeba4da53a5023bc9b6cc31d3008
PUBPOL 2130 Admin <pubpol.21.30@gmail.com>
2025-01-29 00:11:37 -0500

    fix: call find_packages()

--- lib/pubpol2130/setup.py
+++ lib/pubpol2130/setup.py
@@ -4,5 +4,6 @@ setup(
     name="pubpol2130",
     version="0.0.1",
     url="https://github.com/PUBPOL-2130/notebooks",
+    packages=find_packages(),
     install_requires=["census >= 0.8.19", "us >= 2.0.2", "pandas >= 2.2.3"],
 )
