fd756d25392fc784747ec5ef120217b6ba972657
PUBPOL 2130 Admin <pubpol.21.30@gmail.com>
2025-02-13 20:08:54 -0500

    wip: arms transfers, etc.

--- lib/setup.py
+++ lib/setup.py
@@ -5,5 +5,11 @@ setup(
     version="0.0.1",
     url="https://github.com/PUBPOL-2130/notebooks",
     packages=find_packages(),
-    install_requires=["census >= 0.8.19", "us >= 2.0.2", "pandas >= 2.2.3"],
+    install_requires=[
+        "census >= 0.8.19",
+        "us >= 2.0.2",
+        "pandas >= 2.2.3",
+        "gspread_pandas >= 3.3.0",
+        "tqdm >= 4.0.0",
+    ],
 )
