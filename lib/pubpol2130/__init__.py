7db7815345bc45015f6b8d1262a70a0c7cf34943
PUBPOL 2130 Admin <pubpol.21.30@gmail.com>
2025-02-15 23:35:40 -0500

    Productionize notebook: clean up remaining capitals, etc.

--- lib/pubpol2130/__init__.py
+++ lib/pubpol2130/__init__.py
@@ -1,4 +1,4 @@
-from .flows import generate_flow_sheet, google_sheets_credentials
+from .flows import google_sheets_credentials, generate_flow_sheet
 from .pop import condensed_populations
 
-__all__ = ["condensed_populations", "generate_flow_sheet", "google_sheets_credentials"]
+__all__ = ["condensed_populations", "google_sheets_credentials", "generate_flow_sheet"]
